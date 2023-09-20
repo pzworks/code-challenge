27b4d6feb9244f0aa67f5f7cc581ddfe4ed4e1e7
pzworks <piotrziolkowski92@gmail.com>
2023-09-20 21:26:30 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// src/app.ts
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
