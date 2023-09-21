dc0508ce3022de60809c98cfda563b6d252665c4
pzworks <piotrziolkowski92@gmail.com>
2023-09-21 23:17:19 +0200

    set source param as optional

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -2,7 +2,7 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/', (req: any, res: any) => {
+router.get('/:source?', (req: any, res: any) => {
   res.send({ transactions: [] })
 })
 
